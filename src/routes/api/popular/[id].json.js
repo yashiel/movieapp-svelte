0735a8191fb38c85089a044fac48528aeea1b465
Yashi EL <hi@yashiel.com>
2022-02-24 17:28:37 +0530

    Formatting and next page, prev page link added

--- src/routes/api/popular/[id].json.js
+++ src/routes/api/popular/[id].json.js
@@ -1,11 +1,11 @@
-export const get = async function(request){
-    const id = request.params.id;
-    const url = `https://api.themoviedb.org/3/tv/popular?api_key=2dc1a3f5923224ebac1f054d5be0bb40&language=en-US&page=${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
+export const get = async function (request) {
+	const id = request.params.id;
+	const url = `https://api.themoviedb.org/3/tv/popular?api_key=2dc1a3f5923224ebac1f054d5be0bb40&language=en-US&page=${id}`;
+	const res = await fetch(url);
+	const data = await res.json();
 
-    return {
-        status: 200,
-        body: data,
-    }
-}
+	return {
+		status: 200,
+		body: data
+	};
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,45 @@
 <script context="module">
-
-
-export async function load({ params, fetch, session, stuff }) {
-    const url = `./api/popular.json`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    if(response.ok){
-        return {
-            props: {
-                shows : data.results,
-                data,
-            } 
-        }
-    }
-  }
-
-
+	// export const prerender = true;
+	export async function load({ params, fetch, session, stuff }) {
+		const url = `../api/popular.json`;
+		const response = await fetch(url);
+		const data = await response.json();
+		if (response.ok) {
+			return {
+				props: {
+					shows: data.results,
+					data
+				}
+			};
+		}
+	}
 </script>
 
 <script>
 	export let shows;
-    export let data;
+	export let data;
 </script>
 
-<span>Current Page : <b>{data.page}</b></span>  <span>Total Pages : <b>{data.total_pages}</b></span> <span>Total Results : <b>{data.total_results}</b></span> 
-<br>
+<span>Current Page : <b>{data.page}</b></span> <span>Total Pages : <b>{data.total_pages}</b></span>
+<span>Total Results : <b>{data.total_results}</b></span>
+<br />
 
 {#each shows as tv, i}
 	<h1>{tv.original_name}</h1>
-    <h5>Popularity : {tv.popularity}</h5>
-    <h5>Vote Avarage : {tv.vote_average}</h5>
-    <h5>Vote Count : {tv.vote_count}</h5>
-    <hr>
+	<h5>Popularity : {tv.popularity}</h5>
+	<h5>Vote Avarage : {tv.vote_average}</h5>
+	<h5>Vote Count : {tv.vote_count}</h5>
+	<hr />
 {/each}
 
-<a href={`/popular/${data.page += 1}`}>Next Page</a> <br>
-<span>Current Page : <b>{data.page}</b></span>  <span>Total Pages : <b>{data.total_pages}</b></span> <span>Total Results : <b>{data.total_results}</b></span> 
-
+<a sveltekit:prefetch href={`/popular/${data.page + 1}`}>Next Page</a> <br />
+<span>Current Page : <b>{data.page}</b></span> <span>Total Pages : <b>{data.total_pages}</b></span>
+<span>Total Results : <b>{data.total_results}</b></span>
 
 <style>
-    :global(h1) {
-        color: rgb(24, 24, 24);
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 2rem;
-    }
+	:global(h1) {
+		color: rgb(24, 24, 24);
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+		font-size: 2rem;
+	}
 </style>
